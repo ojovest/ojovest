1b65ba8e113d5fde641e996543c6b86530121233
ojovest <director@ojovest.com>
2026-02-01 21:03:17 +0530

    Create gtag.js

---
+++ app/lib/gtag.js
@@ -0,0 +1,11 @@
+export const GA_TRACKING_ID = "G-KZQFTXMEDC";
+
+export const event = ({ action, category, label, value }) => {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  }
+};
